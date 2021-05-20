8f020529ae35ac6f6269d182d52a099be4456c5d
sosaShutup <83962634+sosaShutup@users.noreply.github.com>
2021-05-20 10:17:18 -0500

    Update success.php

--- codeingniter/app/Views/success.php
+++ codeingniter/app/Views/success.php
@@ -5,7 +5,9 @@
 <div class="container">
     <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-        <br><br> <h2 style="color:#0fad00">Success</h2>
+        <br><br> <h1 style="color:#0fad00">{user_name}</h1>
+        <br><br> <h2 style="color:#0fad00">{password}</h2>    
+            
         </div>
     </div>
 </body>
